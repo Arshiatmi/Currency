db2b32970c298bc18918d80977b84a7305eba915
root <root@localhost.localdomain>
2020-02-08 15:56:15 -0500

    Add Bit Coin

--- price.py
+++ price.py
@@ -1,14 +1,45 @@
 import requests
 from bs4 import BeautifulSoup
 
-r = requests.get("https://markets.eghtesadnews.com/69386/%D9%BE%D9%88%D9%86%D8%AF")
+cmd = input("""
+Which One ? 
 
-parser = BeautifulSoup(r.text,'html.parser')
-allD = parser.find_all("span",{"class":"field-content"})
-print("Pound : " + allD[1].get_text())
+	[1] => Dollar
+	[2] => Pound
+	[3] => Bit Coin
+	[4] => All Of Them
+: """)
 
-r = requests.get("https://markets.eghtesadnews.com/69384/%D8%AF%D9%84%D8%A7%D8%B1")
+cmd = int(cmd)
 
-parser = BeautifulSoup(r.text,'html.parser')
-allD = parser.find_all("span",{"class":"field-content"})
-print("Dollar : " + allD[1].get_text())
+def pound():
+	r = requests.get("https://markets.eghtesadnews.com/69386/%D9%BE%D9%88%D9%86%D8%AF")
+	parser = BeautifulSoup(r.text,'html.parser')
+	allD = parser.find_all("span",{"class":"field-content"})
+	print("Pound : " + allD[1].get_text())
+
+def dollar():
+	r = requests.get("https://markets.eghtesadnews.com/69384/%D8%AF%D9%84%D8%A7%D8%B1")
+	parser = BeautifulSoup(r.text,'html.parser')
+	allD = parser.find_all("span",{"class":"field-content"})
+	print("Dollar : " + allD[1].get_text())
+
+def bitCoin():
+	r = requests.get("https://arzdigital.com/coins/bitcoin/")
+	parser = BeautifulSoup(r.text,'html.parser')
+	allD = parser.find_all("div",{"class":"arz-coin-page-data-coin-toman-price"})
+	print("Bit Coin : " + allD[0].find('span').get_text())
+
+def all():
+	pound()
+	dollar()
+	bitCoin()
+
+if cmd == 1:
+	dollar()
+elif cmd == 2:
+	pound()
+elif cmd == 3:
+	bitCoin()
+else:
+	all()
